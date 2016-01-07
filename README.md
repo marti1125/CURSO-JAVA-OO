f29fe2c4f16ac2a75b4ddcc0751ba53df983063b
Gustavo Coronel <gcoronelc@gmail.com>
2016-01-07 13:58:11 -0500

    Actualizando Tema 01

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CURSO-JAVA-OO
 Material del curso de Java Orientado a Objetos
+
+# TEMA 01: CLASES Y OBJETOS
+
+- Abstracción
+- Clase
+- Objeto
+- Implementación
+- Operador new
+- Ejecución de metodos
+- Proyecto ejemplo
+
+
+
+
---
+++ Tema01/AprendiendoJava/src/pe/egcc/modelo/Articulo.java
@@ -0,0 +1,20 @@
+package pe.egcc.modelo;
+
+/**
+ *
+ * @author Eric Gustavo Coronel Castillo
+ * @blog   www.desarrollasoftware.com
+ * @email  gcoronelc@gmail.com
+ */
+public class Articulo {
+  
+  // Variables
+  private String nombre;
+  private double precio;
+  private int stock;
+  
+  
+  
+
+}
+
